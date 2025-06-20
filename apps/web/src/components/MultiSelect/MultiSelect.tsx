a10b8a74c8a202e1529eda4ee487d2808f878219
Jan Jaap van Deursen <j.j.m.vandeursen@gmail.com>
2025-06-20 14:04:07 +0200

    Finishes multi-select component: - adds throttling for filter input - adds divider between selected and selectable options - sets and retrieves items to and from storage - defers rendering of the list element when items are retrieved from storage

--- apps/web/src/components/MultiSelect/MultiSelect.tsx
+++ apps/web/src/components/MultiSelect/MultiSelect.tsx
@@ -1,4 +1,6 @@
+import throttle from 'lodash/throttle';
 import { useEffect, useRef, useState, type ChangeEvent } from 'react';
+import { getStorageProvider } from '../../lib/storage';
 import { Item } from '../Item/Item';
 import Styles from './MultiSelect.module.css';
 
@@ -6,93 +8,146 @@ interface MultiSelectProps {
   items: Array<string>;
 }
 
+const storageProvider = getStorageProvider();
+
 const MultiSelect = ({ items }: MultiSelectProps) => {
-  const [preferredItems, setPreferredItems] = useState<Array<string>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [preselectedItems, setPreselectedItems] = useState<Array<string>>([]);
   const [selectedItems, setSelectedItems] = useState<Array<string>>([]);
   const [unselectedItems, setUnselectedItems] = useState<Array<string>>(items);
   const [searchValue, setSearchValue] = useState<string>('');
   const itemsContainer = useRef<HTMLUListElement>(null);
 
-  const onSearch = (event: ChangeEvent<HTMLInputElement>) => {
-    const searchValue = event.target.value.trim();
+  const applyFilter = throttle(
+    (inputValue: string) => {
+      if (!inputValue) {
+        setUnselectedItems(items);
+        return;
+      }
 
-    setSearchValue(searchValue);
+      const itemsMatching = items.filter((item) => item.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase()));
 
-    if (!searchValue) return;
+      setUnselectedItems(itemsMatching);
+    },
+    500,
+    { leading: false, trailing: true },
+  );
 
-    const itemsMatching = items.filter((item) => item.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()));
+  const onFilter = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value.trim();
 
-    setUnselectedItems(itemsMatching);
+    setSearchValue(inputValue);
+
+    applyFilter(inputValue);
   };
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (preferredItems.length === 0) {
+    setSearchValue('');
+
+    if (preselectedItems.length === 0) {
       return false;
     }
 
-    setSelectedItems((prev) => [...prev, ...preferredItems].sort((a, b) => a.localeCompare(b)));
-    setPreferredItems([]);
+    setSelectedItems((prev) => {
+      const updatedSelectedItems = [...prev, ...preselectedItems].sort((a, b) => a.localeCompare(b));
+
+      storageProvider.setItem('selectedItems', JSON.stringify(updatedSelectedItems));
+
+      return updatedSelectedItems;
+    });
+
+    setPreselectedItems([]);
+
+    setUnselectedItems(() => items.filter((item) => !preselectedItems.includes(item)));
 
     if (itemsContainer.current) {
       itemsContainer.current.scrollTo({ top: 0, behavior: 'smooth' });
     }
   };
 
-  const onSelectItem = (event: ChangeEvent<HTMLInputElement>) => {
+  const onPreselectItem = (event: ChangeEvent<HTMLInputElement>) => {
     const selectedItem = event.target.value;
     const isChecked = event.target.checked;
 
     if (isChecked) {
-      setPreferredItems((prev) => [...prev, selectedItem]);
+      setPreselectedItems((prev) => [...prev, selectedItem]);
     } else {
-      setPreferredItems((prev) => prev.filter((item) => item !== selectedItem));
+      setPreselectedItems((prev) => prev.filter((item) => item !== selectedItem));
     }
   };
 
   const onDeselectItem = (event: ChangeEvent<HTMLInputElement>) => {
     const deselectedItem = event.target.value;
 
-    setSelectedItems((prev) => prev.filter((item) => item !== deselectedItem));
+    setSelectedItems((prev) => {
+      const updatedSelectedItems = prev.filter((item) => item !== deselectedItem);
+
+      storageProvider.setItem('selectedItems', JSON.stringify(updatedSelectedItems));
+
+      return updatedSelectedItems;
+    });
   };
 
   useEffect(() => {
-    setUnselectedItems(items.filter((item) => !selectedItems.includes(item)));
-  }, [selectedItems, items]);
+    const itemsFromStorage = storageProvider.getItem('selectedItems');
+
+    if (!itemsFromStorage) {
+      setUnselectedItems(items);
+    } else {
+      try {
+        const parsedPreferences = JSON.parse(itemsFromStorage) as Array<string>;
+
+        setSelectedItems(parsedPreferences);
+        setUnselectedItems(items.filter((item) => !parsedPreferences.includes(item)));
+      } catch (error) {
+        // invalid JSON, fail silently
+        console.error('Failed to parse selected items from storage:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  }, [items]);
 
   return (
-    <>
-      <h2>Select Multiple Options</h2>
+    <search>
+      <form className={Styles.multiSelect__form} onSubmit={onSubmit}>
+        <input
+          className={Styles.multiSelect__input}
+          onChange={onFilter}
+          placeholder="Search..."
+          type="search"
+          value={searchValue}
+        />
 
-      <search>
-        <form className={Styles.multiSelect} onSubmit={onSubmit}>
-          <input
-            className={Styles.multiSelect__input}
-            onChange={onSearch}
-            placeholder="Search..."
-            type="search"
-            value={searchValue}
-          />
+        <ul className={Styles.multiSelect__list} ref={itemsContainer}>
+          {!loading && (
+            <>
+              {selectedItems.map((item: string) => (
+                <li key={item} value={item} className={Styles.multiSelect__listItem}>
+                  <Item item={item} onSelectItem={onDeselectItem} selected />
+                </li>
+              ))}
 
-          <ul className={Styles.multiSelect__list} ref={itemsContainer}>
-            {selectedItems.map((item: string) => (
-              <li key={item} value={item} className={Styles.multiSelect__listItem}>
-                <Item item={item} onSelectItem={onDeselectItem} selected />
-              </li>
-            ))}
+              {selectedItems.length > 0 && (
+                <li>
+                  <hr className={Styles.multiSelect__divider} />
+                </li>
+              )}
 
-            {unselectedItems.map((item: string) => (
-              <li key={item} value={item} className={Styles.multiSelect__listItem}>
-                <Item item={item} onSelectItem={onSelectItem} selected={preferredItems.includes(item)} />
-              </li>
-            ))}
-          </ul>
+              {unselectedItems.map((item: string) => (
+                <li key={item} value={item} className={Styles.multiSelect__listItem}>
+                  <Item item={item} onSelectItem={onPreselectItem} selected={preselectedItems.includes(item)} />
+                </li>
+              ))}
+            </>
+          )}
+        </ul>
 
-          <input type="submit" value="Apply" />
-        </form>
-      </search>
-    </>
+        <input className={Styles.multiSelect__submit} type="submit" value="Apply" />
+      </form>
+    </search>
   );
 };
 
